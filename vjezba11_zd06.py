bcb26c7dc2fcf73ababdd2fbcc2a80d8a888c447
kuste <36107643+kuste@users.noreply.github.com>
2018-02-03 16:27:12 +0100

    Update vjezba11_zd06.py

--- vjezba11_zd06.py
+++ vjezba11_zd06.py
@@ -23,3 +23,4 @@ for i in range(10):
     end_fill()
     start*=0.9    
     
+'''Napišite program vjezba11_zd06.py koji crta deset koncentričnih kružnica, od kojih najveda ima radijus duljine 300 piksela, a svaka iduda ima radijus za 10% manji od prethodne. Svaka kružnica mora biti ispunjena jednom nijansom sive boje, pri čemu najveda kružnica mora biti ispunjena bijelom bojom, a svaka iduda za 5% tamnijom nijansom sive boje od prethodne.'''
